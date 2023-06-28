a38c09c9d7bf5503b50aeac9515ee87051a55fcd
Jean Carlos Tavares <carlosjean.t@gmail.com>
2023-06-27 23:36:44 -0300

    Atividade ebac modulo 22

---
+++ src/Stream/Mulheres.java
@@ -0,0 +1,14 @@
+package Stream;
+import java.util.ArrayList;
+
+public class Mulheres {
+	public static void imprimirMulheres(ArrayList<String> nomes) {
+        ArrayList<String> mulheres = new ArrayList<>(nomes.stream()
+                .filter(nome -> nome.split("-")[1].trim().equalsIgnoreCase("m"))
+                .map(nome -> nome.split("-")[0].trim())
+                .toList());
+
+        System.out.println("Mulheres cadastradas: " + mulheres);
+    }
+}
+
